602820051d39b3702d32a925edd8c28d76eb7e96
laricko <ttttmmm6432@gmail.com>
2023-07-25 23:54:05 -0700

    add temporary cors middleware

--- src/middleware.py
+++ src/middleware.py
@@ -14,5 +14,21 @@ class AuthMiddleware:
         return self.app(environ, start_response)
 
 
+class CorsMiddleware:
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        def new_start_response(status: str, headers: list[tuple]):
+            # NOTE: In a future this should be handeled properly
+            headers.append(("Access-Control-Allow-Origin", "*"))
+            headers.append(("Access-Control-Allow-Headers", "*"))
+            headers.append(("Access-Control-Allow-Methods", "*"))
+            return start_response(status, headers)
+
+        return self.app(environ, new_start_response)
+
+
 def add_middlewares(app: Flask):
+    app.wsgi_app = CorsMiddleware(app.wsgi_app)
     app.wsgi_app = AuthMiddleware(app.wsgi_app)
